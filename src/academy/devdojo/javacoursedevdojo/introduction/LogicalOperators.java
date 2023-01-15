8f9e7234a5406ee71b5fb9f7777a539cba7ebbaf
Carolina <carolsalgadoguadanhini@gmail.com>
2023-01-15 18:05:25 -0300

    add class 18 content: logical operator or

--- src/academy/devdojo/javacoursedevdojo/introduction/LogicalOperators.java
+++ src/academy/devdojo/javacoursedevdojo/introduction/LogicalOperators.java
@@ -2,6 +2,7 @@ package academy.devdojo.javacoursedevdojo.introduction;
 
 public class LogicalOperators {
     public static void main(String[] args) {
+        //* And (&&)
         int age = 19;
         float salary = 3550F;
         boolean isAgeGreaterThan30 = age >= 30 && salary >= 4612;
@@ -9,5 +10,15 @@ public class LogicalOperators {
 
         System.out.println("Age is greater than 30: " + isAgeGreaterThan30);
         System.out.println("Age is less than 30: " + isAgeLessThan30);
+
+        //* Or (||)
+        double totalCheckingAccount = 200;
+        double totalSavingsAccount = 8000;
+
+        float playstation5Value = 5000;
+        boolean isBuyable = totalCheckingAccount > playstation5Value || totalSavingsAccount > playstation5Value;
+
+        System.out.println(isBuyable + ", I can buy a playstation 5");
+        // true
     }
 }
