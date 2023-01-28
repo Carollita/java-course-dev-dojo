9646428be913ce017d366d0cff694ad1762e68fe
Carolina <carolsalgadoguadanhini@gmail.com>
2023-01-28 02:04:55 -0300

    add class 28: loop statement for exercise

---
+++ src/academy/devdojo/javacoursedevdojo/introduction/ExerciseLoopstatementFor.java
@@ -0,0 +1,12 @@
+package academy.devdojo.javacoursedevdojo.introduction;
+
+public class ExerciseLoopstatementFor {
+    public static void main(String[] args) {
+        // Print even number
+        for(int i=0; i <= 100; i ++) {
+            if(i % 2 == 0 && i!= 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
