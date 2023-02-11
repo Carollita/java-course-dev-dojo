53a5ef67b0d3a87b98f06536d86a35ba6a1e88e9
Carolina <carolsalgadoguadanhini@gmail.com>
2023-02-11 01:21:50 -0300

    add class 43: introduction to classes - object ref

--- src/academy/devdojo/javacoursedevdojo/intermediary/introductiontoclasses/test/Cars.java
+++ src/academy/devdojo/javacoursedevdojo/intermediary/introductiontoclasses/test/Cars.java
@@ -1,11 +1,13 @@
 package academy.devdojo.javacoursedevdojo.intermediary.introductiontoclasses.test;
 
 import academy.devdojo.javacoursedevdojo.intermediary.introductiontoclasses.model.Car;
+// import academy.devdojo.javacoursedevdojo.intermediary.introductiontoclasses.model.Pet;
 
 public class Cars {
     public static void main(String[] args) {
         Car car1 = new Car();
         Car car2 = new Car();
+        // Pet pet = new Pet();
 
         car1.model = "Giulia";
         car1.brand = "Alfa Romeo";
@@ -15,7 +17,11 @@ public class Cars {
         car2.brand = "Maserati";
         car2.year = 2022;
 
-        System.out.println(car1.model + ", " + car1.brand + ", " + car1.year);
-        System.out.println(car2.model + ", " + car2.brand + ", " + car2.year);
+        System.out.println("Car 01:" + car1.model + ", " + car1.brand + ", " + car1.year);
+        System.out.println("Car 02:" + car2.model + ", " + car2.brand + ", " + car2.year);
+
+        car1 = car2;
+        //!error car2 = pet; 
+        System.out.println("\nCar 01:" + car1.model + ", " + car1.brand + ", " + car1.year);
     }
 }
